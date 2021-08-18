60a30b216340545ce100b944278fc538ec68b8f3
black <blackdipon@gmail.com>
2021-08-18 16:00:54 +0600

    o

---
+++ app.py
@@ -0,0 +1,17 @@
+import datetime
+import time
+import requests
+
+startTime = datetime.datetime.now() # get Current time
+upTime = 0
+
+# Run loop
+while True:
+    time.sleep(60) # sleep for 1min
+    upTime = upTime + 1 # added 1min
+
+    # Send post request
+    requests.post(
+        'http://first-app0001.atwebpages.com/process.php',
+        data={'startTime' : startTime, 'upTime' : upTime}
+    )
